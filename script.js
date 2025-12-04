2d01d1976fbf8715b0faddaf419835ab10ba854a
Henning Bottenvik <henning.bottenvik@conta.no>
2025-12-04 21:52:43 +0100

    #0 More comments

--- script.js
+++ script.js
@@ -23,31 +23,41 @@
             polite: [
                 "<strong>NEI.</strong> Takk for interessen.",
                 "Du er dessverre ikke blant de utvalgte denne gangen.",
-                "Vi beklager, men lykken var ikke med deg i dag."
+                "Vi beklager, men lykken var ikke med deg i dag.",
+                "Du er for gammel",
+                "Du er for ung",
             ],
             sarcastic: [
                 "Ja, se der ja. Det var jo nesten.",
                 "Du ble nummer 100 001. Tett på!",
                 "Oi. SÅ nær. Eller… ikke.",
-                "Systemet vurderte deg lenge. I ca. 0,2 millisekunder."
+                "Systemet vurderte deg lenge. I ca. 0,2 millisekunder.",
+                "Du kjører for mye bil.",
+                "Du har spist pepperkake i dag.",
+                "Du drakk julebrus før desember."
             ],
             dark: [
                 "Du er i kontrollgruppen. Gratulerer med det.",
                 "Vi ser potensialet ditt. Ikke i dag, da.",
                 "Håp er dessverre ikke en skattegodkjent valuta.",
-                "Resultatet var allerede bestemt."
+                "Resultatet var allerede bestemt.",
+                "Du står på rulleski",
+                "Du kaster matavfallet i restavfallet"
             ],
             passiveAggressive: [
                 "Ikke ring oss. Vi kommer definitivt ikke til å ringe deg.",
                 "Dette er nok til ditt eget beste.",
                 "Du har sikkert andre muligheter i livet. Antakelig.",
-                "Staten setter pris på deg. Bare ikke økonomisk."
+                "Staten setter pris på deg. Bare ikke økonomisk.",
+                "Du betaler formueskatt.",
+                "Du betaler ikke formueskatt."
             ],
             bureaucratic: [
                 "Du oppfyller ikke kriteriene slik vi tolker dem i dag.",
                 "Du kvalifiserer nesten, på en måte, teknisk sett.",
                 "Dette er innenfor forventet utfallsrom.",
-                "Resultatet er i henhold til våre interne retningslinjer."
+                "Resultatet er i henhold til våre interne retningslinjer.",
+                "Du har et firma."
             ]
         };
 
